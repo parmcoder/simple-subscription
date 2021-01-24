137785fc0cfa9978093d3145be805fff93503395
RecursivePerson <possawat2017@hotmail.com>
2021-01-24 20:26:18 +0700

    now the site should work

--- scripts.js
+++ scripts.js
@@ -1,23 +1,45 @@
-
 function validateEmail(str) {
-    var emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; 
-    // The email validator, https://www.w3resource.com/javascript/form/email-validation.php
-    
-    return emailPattern.test(str);
+  var emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  // The email validator, https://www.w3resource.com/javascript/form/email-validation.php
+
+  return emailPattern.test(str);
+}
+
+function showError() {
+  document.getElementById("email").style.outline = "none";
+  document.getElementById("email").style.border = "1px solid red";
+  document.getElementById("error-text").innerHTML = "A valid email is required";
+}
+
+function ok() {
+  document.getElementById("email").style.outline = "none";
+  document.getElementById("email").style.border = "1px solid black";
+  document.getElementById("error-text").innerHTML = "";
+}
+
+function showSuccess(){
+    document.getElementById("forum").innerHTML = document.getElementById(
+        "success",
+      ).innerHTML;
+}
+
+function validator() {
+  var email = document.getElementById("email").value;
+  if (validateEmail(email)) {
+    showSuccess();
+    // simply replace with the success component
+  } else {
+    showError();
   }
+}
 
 function onFocus() {
-    if (document.activeElement.tagName != "INPUT") {
-      var email = document.getElementById("email").value;
-      if (!validateEmail(email)) {
-        document.getElementById("email").style.outline = "none";
-        document.getElementById("email").style.border = "1px solid red";
-        document.getElementById("error-text").innerHTML =
-          "A valid email is required";
-      } else {
-        document.getElementById("email").style.outline = "none";
-        document.getElementById("email").style.border = "1px solid black";
-        document.getElementById("error-text").innerHTML = "";
-      }
+  if (document.activeElement.tagName != "INPUT") {
+    var email = document.getElementById("email").value;
+    if (validateEmail(email)) {
+      ok();
+    } else {
+      showError();
     }
   }
+}
